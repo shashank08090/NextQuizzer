d6ff95bc7eb7b1613426e0e2fbadea05350b1d0d
Shashank <shashank08090@gmail.com>
2022-10-20 14:48:28 +0530

    signup successful

---
+++ model/schema.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+const userSchema = mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+module.exports = mongoose.models.User || mongoose.model("User", userSchema);
