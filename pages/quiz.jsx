67d859e47bceeda53129fe5ad69e0c02ea913bb7
Shashank <shashank08090@gmail.com>
2022-10-25 17:06:52 +0530

    changes in material ui

--- pages/quiz.jsx
+++ pages/quiz.jsx
@@ -1,18 +1,42 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Header from './Header/Header'
 const Quiz = ({ users }) => {
   console.log(users);
   return (
-    <div>
-      {/* <h1>{message}</h1> */}
+    <>
+    <Header></Header>
+    <h1 style={{textAlign:"center"}}>User data  from API </h1>
+    <div className="user_card">
+      
+      
+      
       {users?.map((user) =>{
         return (
-          <div key={user?.id}>
-            <p>{user?.name}</p>
-            <p>{user?.email}</p>
+          <div className='card_container' key={user?.id}>
+          <Card sx={{minwidth:275}} key={user?.id} variant="outlined" >
+           <div className='card_content'>
+           <CardContent>
+             <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+             <p>{user?.name}</p>
+        </Typography>
+        <Typography variant="h5" component="div">
+        <p>{user?.email}</p>
+        </Typography>
+            </CardContent>
+           </div>
+            </Card>
             </div>
         )
       })}
-      <h1>is there a message</h1>
+      
     </div>
+    </>
   );
 };
 
