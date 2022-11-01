1d0601a503f0a1dd848f6a2d95ebf4df6e9a32f6
Shashank <shashank08090@gmail.com>
2022-11-01 17:52:21 +0530

    bug fixing

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Welcome />
-      <Quiz />
+      {/* <Quiz /> */}
 
       {/* </Router> */}
     </div>
